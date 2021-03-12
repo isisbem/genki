f9d4abb3da50201a89edaacece2fab5d741694f5
Antonio I <aiorio2004@gmail.com>
2021-03-12 16:42:33 +0100

    Aggiunto pseudo-footer
    Ci ho provato

--- components/main.php
+++ components/main.php
@@ -27,6 +27,14 @@
                 </div>
             </div>
         </nav>
+    
+        <footer class="footer-dark bg-primary fixed-bottom  w-100 shadow p-3 rounded">
+            <div class="container">
+                <button type="button" class="btn btn-primary text-decoration-underline rounded">eni ar iu ochei</button>
+            </div>
+        </footer>
+
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
     </body>
 </html>
+
